0355a06faecc09fba8bb8008b10d7e00c6de31d5
tuguzT <timurka.tugushev@gmail.com>
2021-11-11 17:30:46 +0300

    Set minimum window size, add title to main view

--- src/main/kotlin/io/github/tuguzt/sql/Application.kt
+++ src/main/kotlin/io/github/tuguzt/sql/Application.kt
@@ -1,8 +1,17 @@
 package io.github.tuguzt.sql
 
+import javafx.stage.Stage
 import tornadofx.*
 
-class Application : App(MainView::class)
+class Application : App(MainView::class) {
+    override fun start(stage: Stage) {
+        with(stage) {
+            minWidth = 300.0
+            minHeight = 150.0
+        }
+        super.start(stage)
+    }
+}
 
 fun main(args: Array<String>) {
     launch<Application>(args)
--- src/main/kotlin/io/github/tuguzt/sql/MainView.kt
+++ src/main/kotlin/io/github/tuguzt/sql/MainView.kt
@@ -2,7 +2,7 @@ package io.github.tuguzt.sql
 
 import tornadofx.*
 
-class MainView : View() {
+class MainView : View("SQL Practice 5") {
     override val root = vbox {
         button("Press me")
         label("Waiting")
